3bc098e7c942bb3b010f6f1d1a6123381e98eefe
YoungJun Lee <yjlee@nshc.net>
2023-09-11 14:56:54 +0900

    fix symbolic link path

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -12,7 +12,9 @@ mkdir LxShield
 cd LxShield
 
 LXSHIELD_ZIP_NAME="Xcode1431_Test_14.3.1_1.14.3.3"
-LXSHIELD_CLOUD_PATH=`pwd`/LxShield/${LXSHIELD_ZIP_NAME}
+
+# LXSHIELD_CLOUD_PATH=/Volumes/workspace/repository/ci_scripts/LxShield/Xcode1431_Test_14.3.1_1.14.3.3
+LXSHIELD_CLOUD_PATH=`pwd`/${LXSHIELD_ZIP_NAME}
 
 wget http://everythinghackable.com/temp_nshc/${LXSHIELD_ZIP_NAME}.zip
 unzip ${LXSHIELD_ZIP_NAME}.zip
