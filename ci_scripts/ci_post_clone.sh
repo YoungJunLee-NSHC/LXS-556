a36165fd4a13e58b7918ec09a21c0bb1df082bb9
YoungJun Lee <yjlee@nshc.net>
2023-09-11 11:41:34 +0900

    Add Xcode Cloud Scripts

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  LXS_556
+#
+#  Created by YoungJun Lee on 2023/09/11.
+#  
+
+mkdir LxShield
+cd LxShield
+
+wget http://everythinghackable.com/temp_nshc/Xcode1431_Test_14.3.1_1.14.3.3.zip
+unzip Xcode1431_Test_14.3.1_1.14.3.3.zip
+
+ls -al
